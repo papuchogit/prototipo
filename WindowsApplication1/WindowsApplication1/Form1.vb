ae90548a929b69e9282de67dff056348c53a053e
Luis Yucailla <sonoralospaladines@hotmail.com>
2015-07-23 01:36:53 -0500

    una primera prueba

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        hola holaaaa mundo
+        esto es una prueba
+    End Sub
+End Class
