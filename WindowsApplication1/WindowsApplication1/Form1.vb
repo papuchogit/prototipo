02853b6f15508133afc59c3dc93fe417dfe2aa4d
Luis Yucailla <sonoralospaladines@hotmail.com>
2015-07-23 01:40:11 -0500

    boton

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        hola holaaaa mundo
-        esto es una prueba
+        
     End Sub
 End Class
